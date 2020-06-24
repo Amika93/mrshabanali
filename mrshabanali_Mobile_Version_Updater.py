8c5131c711f1507a53482a5c06ad3873851486a2
Iman Nazari <imaan.nazarii@gmail.com>
2020-06-24 17:30:17 +0430

    Create functions for main codes

--- mrshabanali_Mobile_Version_Updater.py
+++ mrshabanali_Mobile_Version_Updater.py
@@ -2,6 +2,9 @@ from requests import get
 from bs4 import BeautifulSoup
 import os
 
+headers = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36'}
+
 
 def urlEx(url):
     newList = []
@@ -13,6 +16,7 @@ def urlEx(url):
         link = item.find('a')
         newList.insert(0, link['href'])
     return newList
+
 
 def newLinksAgg(url):
     newLinks = []
@@ -33,58 +37,63 @@ def newLinksAgg(url):
             newLinks = linksToCheck + newLinks
             return newLinks
 
-pageToCheck = 'http://mrshabanali.com/page/'
-linksToAppend = []
-pageNumber = 1
-T = True
-while T:
-    nwlst = newLinksAgg(pageToCheck + str(pageNumber))
-    if nwlst == None:
-        print('Your List is Up To Date!')
-        T = False
-    elif len(nwlst) == 10:
-        linksToAppend = nwlst + linksToAppend
-        pageNumber += 1
+
+def update_list():
+    pageToCheck = 'http://mrshabanali.com/page/'
+    linksToAppend = []
+    pageNumber = 1
+
+    while True:
+        nwlst = newLinksAgg(pageToCheck + str(pageNumber))
+        if nwlst == None:
+            print('Your List is Up To Date!')
+            break
+        elif len(nwlst) == 10:
+            linksToAppend = nwlst + linksToAppend
+            pageNumber += 1
+        else:
+            print('Link Found, Your List is Updating...')
+            linksToAppend = nwlst + linksToAppend
+            break
+
+    if len(linksToAppend) > 0:
+        with open('mrshabanali/list.txt', 'a') as f:
+            for item in linksToAppend:
+                f.write("%s\n" % item)
+    print('Done!')
+
+
+def update_files():
+    mobileFiles = os.listdir('mrshabanali/mobilePages')
+    mobileNames = []
+    for f in mobileFiles:
+        mobileNames.append(int(f[:-4]))
+    if mobileNames:
+        lastMobileFileNumber = max(mobileNames)
     else:
-        print('Link Found, Your List is Updating...')
-        linksToAppend = nwlst + linksToAppend
-        T = False
+        lastMobileFileNumber = 0
 
-if len(linksToAppend) > 0:
-    with open('mrshabanali/list.txt', 'a') as f:
-        for item in linksToAppend:
-            f.write("%s\n" % item)
-print('Done!')
+    with open('mrshabanali/list.txt', 'r') as f:
+        lines = f.readlines()
+        if lastMobileFileNumber == len(lines):
+            print('Your Mobile List Is Up To Date!')
+        elif lastMobileFileNumber < len(lines):
+            i = lastMobileFileNumber + 1
+            for l in range(lastMobileFileNumber, len(lines)):
+                response = get(str(lines[l]), headers=headers)
+                response.encoding = 'utf-8'
+                cleanHTML = BeautifulSoup(response.text, 'html.parser')
+                fileName = 'mrshabanali/mobilePages/' + str(i) + '.txt'
+                with open(fileName, 'w', encoding="utf-8") as file:
+                    file.write(str(cleanHTML))
+                    print(fileName + ' added to mobilePages!')
+                i += 1
 
-mobileFiles = os.listdir('mrshabanali/mobilePages')
+            print('Done!')
+        else:
+            print('No New Mobile Files!')
 
-mobileNames = []
 
-for f in mobileFiles:
-    mobileNames.append(int(f[:-4]))
-if mobileNames:
-    lastMobileFileNumber = max(mobileNames)
-else:
-    lastMobileFileNumber = 0
-
-with open('mrshabanali/list.txt', 'r') as f:
-    lines = f.readlines()
-    if lastMobileFileNumber == len(lines):
-        print('Your Mobile List Is Up To Date!')
-    elif lastMobileFileNumber < len(lines):
-        i = lastMobileFileNumber + 1
-        for l in range(lastMobileFileNumber, len(lines)):
-            headers = {
-                'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36'}
-            response = get(str(lines[l]), headers=headers)
-            response.encoding = 'utf-8'
-            cleanHTML = BeautifulSoup(response.text, 'html.parser')
-            fileName = 'mrshabanali/mobilePages/' + str(i) + '.txt'
-            with open(fileName, 'w', encoding="utf-8") as file:
-                file.write(str(cleanHTML))
-                print(fileName + ' added to mobilePages!')
-            i += 1
-
-        print('Done!')
-    else:
-        print('No New Mobile Files!')
+if __name__ == '__main__':
+    update_list()
+    update_files()
